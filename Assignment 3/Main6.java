8a22bf8e99af8f8417545e319dc5fa26b5f8cc56
dhumalabhi <113801863+dhumalabhi@users.noreply.github.com>
2022-10-17 00:14:31 +0530

    Add files via upload

---
+++ Assignment 3/Main6.java
@@ -0,0 +1,15 @@
+/*
+6. A bookshop maintains the inventory of books that are being sold at the shop. The list includes details such 
+as author, title, publisher, cost and stock position. Whenever a customer wants a book, the sales person 
+inputs the title and author and the system searches the list and displays whether it is available or not. If it is 
+not, an appropriate message is displayed. If it is, then the system displays the book details and details and 
+requests for the number of copies required. If the required copies are available, the total cost of the 
+requested copies is displayed, otherwise the message requires copies not in stock is displayed. Design a 
+system using a class called Book with suitable member methods and constructors. 
+*/
+
+public class Main6{
+	public static void main(String[] args){
+		
+	}
+}
